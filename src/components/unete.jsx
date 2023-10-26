84e219ef9043c92aeeacaf2ac6c0670899a8d25d
yessenia millar <yesseniamillar@gmail.com>
2023-10-25 21:22:22 -0300

    agrego unete

--- src/components/unete.jsx
+++ src/components/unete.jsx
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+//import Container from '@mui/material/Container';
 import Carrusel from '../components/carrusel'
 
 export function Unete() {
